486b77e9a15e62d49f5f821e9c0e4e6e57a93652
MarcoPradoL <marco.prado.lopez@gmail.com>
2014-06-11 23:55:26 -0500

    Versión inicial

---
+++ estimador_calidad/src/main/java/mx/cimat/marco/AtributoCalidad/Atributo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.cimat.marco.AtributoCalidad;
+
+/**
+ *
+ * @author QUARKSOFT
+ */
+public enum Atributo {
+        DISPONIBILIDAD, 
+        RENDIMIENTO;
+}
